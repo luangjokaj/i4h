03dca92aea8d667b7d2e3842142424aa6c7598f2
Luan Gjokaj <luan@riangle.com>
2018-07-01 13:50:49 +0200

    Pollyfills.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PrismicApp from './PrismicApp';
+import 'babel-polyfill';
 
 ReactDOM.render(<PrismicApp />, document.getElementById('app'));
