28df9b5eae695f7529949b6f97095d9f814ad180
Luan Gjokaj <luan@riangle.com>
2018-09-09 00:07:20 +0200

    Fix SVG logo render

--- src/assets/svg/Pictogram.js
+++ src/assets/svg/Pictogram.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 function Pictogram() {
 	return (
-		<svg xmlns="http://www.w3.org/2000/svg" width="100" height="87" viewBox="0 0 100 87">
-			<path fillRule="evenodd" d="M94,44 L44,130.485221 L144,130.485221 L94,44 L94,44 Z M77.3551571,92.0778565 L93.9899275,120.85094 L110.624698,92.0778565 L77.355541,92.0778565 L94,63.2886787 L127.278606,120.85094 L60.7203868,120.85094 L77.355541,92.0778565 Z" transform="translate(-44 -44)"/>
+		<svg xmlns="http://www.w3.org/2000/svg" width="402" height="347" viewBox="0 0 402 347">
+			<g fillRule="evenodd">
+				<path d="M200.565657,0 L401.131313,346.919302 L0,346.919302 L200.565657,0 Z M200.565657,77.3729302 L67.0707071,308.273186 L334.056566,308.273186 L200.565657,77.3729302 Z" />
+				<polyline points="267.253 192.855 200.525 308.273 133.798 192.855 267.253 192.855" />
+			</g>
 		</svg>
-	)
+	);
 }
 
 export default Pictogram;
